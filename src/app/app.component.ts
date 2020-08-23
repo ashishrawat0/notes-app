67cd1b8f47a48658d39aedb0fd0cac7347abc52f
ashishrawat0 <rawatchd@hotmail.com>
2020-08-23 22:42:26 +0530

    basic add functionality and layout created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { v4 as uuidv4 } from 'uuid';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'notes-app';
-}
+  constructor(private _snackBar: MatSnackBar) { }
+  message = "Please add title"
+  text = "";
+  notes = 'New note';
+  notesForm = new FormGroup({
+    title: new FormControl(''),
+    content: new FormControl(''),
+  });
+
+  notesData = []
+
+  noteInfo(note) {
+    let details = note.target.value
+    if (details) {
+      this.notes = note.target.value
+    }
+    else {
+      this.notes = "new Note"
+    }
+  }
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 2000,
+    });
+  }
+
+    addNote() {
+      let title: String = this.notesForm.value.title
+      let content: String = this.notesForm.value.content
+      let note = {}
+      if (title) {
+        let id = uuidv4();
+        note['id'] = id
+        note["title"] = title;
+        note["content"] = content;
+        this.notesData.push(note)
+        console.log(this.notesData)
+      }
+      else {
+        this.openSnackBar(this.message,"Close")
+      }
+    }
+
+    viewNote(note){
+      console.log(note)
+    }
+
+  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatCardModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
