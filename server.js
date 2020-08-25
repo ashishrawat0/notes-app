97ef5da2098e4e79cc29c025e7ed6b60a36285f8
ashishrawat0 <rawatchd@hotmail.com>
2020-08-25 22:57:37 +0530

    corrected path

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directorsady
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/notes-app'));
 
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/notes-app/index.html'));
